b9bca05f6a18b8dcf3614f4b9e0abb812f0b070e
Chukwualuka Chiama <lukaschiama@gmail.com>
2019-03-27 05:37:10 +0100

    Create client login page

---
+++ client-login/script.js
@@ -0,0 +1,17 @@
+function validateForm() {
+  event.preventDefault();
+  const email = document.forms["loginClient"]["email"].value;
+  const password = document.forms["loginClient"]["password"].value;
+
+  const error = document.getElementById("form-error");
+
+  if (email === "") {
+    error.innerHTML = "Please enter your email address";
+    return null;
+  }
+
+  if (password === "") {
+    error.innerHTML = "Please enter a password";
+    return null;
+  }
+}
