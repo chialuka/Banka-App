e324fd50a567b1fb4da3eac96d9f7c50709b78c3
Chukwualuka Chiama <lukaschiama@gmail.com>
2019-03-28 01:10:18 +0100

    Create dashboard to view all accounts

---
+++ staff-dashboard/script.js
@@ -0,0 +1,22 @@
+(function(){
+  const accounts = document.getElementById("accounts")
+  const user = localStorage.getItem("staffToken");
+
+  if (!user) {
+    window.location.href = "../staff-login/index.html"
+  }
+
+  const clients = JSON.parse(localStorage.getItem("clientToken"));
+
+  clients.map(function(item) {
+    const ul = document.createElement("ul");
+    ul.setAttribute("class", "list")
+    accounts.appendChild(ul)
+    if (item.accNumber) {
+      const li = document.createElement("li");
+      li.setAttribute("class", "item")
+      li.innerHTML = item.accNumber;
+      ul.appendChild(li)
+    }
+  })
+})();
