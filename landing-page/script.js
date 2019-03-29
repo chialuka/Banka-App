79d3c84985a3f46b039454d4c35c87f82032412c
Chukwualuka Chiama <lukaschiama@gmail.com>
2019-03-29 16:06:10 +0100

    Change background and image resolution

--- landing-page/script.js
+++ landing-page/script.js
@@ -1,10 +1,16 @@
-const slides = document.querySelectorAll("#slider .slide");
+const slides = document.querySelectorAll(".slider .slide");
 let currentSlide = 0;
+const menuItems = document.querySelector(".menu-items");
 carousel()
 
 function carousel() {
   slides[currentSlide].className = "slide";
   currentSlide = (currentSlide+1)%slides.length;
   slides[currentSlide].className = "slide showing";
-  setTimeout(carousel, 5000);
+  setTimeout(carousel, 3000);
+}
+
+function menuFunction(menu) {
+  menu.classList.toggle("change");
+  menuItems.style.display = menuItems.style.display === "" || menuItems.style.display === "none" ? "block" : "none";
 }
