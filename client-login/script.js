3aedcf4b0bfb5f0d0acc409f2e481a4b74fe7c7f
Chukwualuka Chiama <lukaschiama@gmail.com>
2019-04-16 13:19:06 +0100

    improve dashboard

--- client-login/script.js
+++ client-login/script.js
@@ -1,30 +1,50 @@
-(function(){
-  const email = localStorage.getItem("loggedInUser");
+(function() {
+  const email = localStorage.getItem('loggedInUser');
   if (email) {
-    window.location.href = "../client-dashboard/index.html"
+    window.location.href = '../client-dashboard/index.html';
   }
 })();
 
 function validateForm() {
   event.preventDefault();
-  const email = document.forms["login-client"]["email"].value;
-  const password = document.forms["login-client"]["password"].value;
+  const email = document.forms['login-client']['email'].value;
+  const password = document.forms['login-client']['password'].value;
+  const role = document.forms['login-client']['roles'].value;
 
-  const error = document.getElementById("form-error");
+  const error = document.getElementById('form-error');
 
-  const clientToken = JSON.parse(localStorage.getItem("clientToken")) || [];
-  if (clientToken.some(item => item["Email"] === email)) {
-    if (clientToken.find(item => item["Password"] === password)) {
-      const client = clientToken.find(x => x["Email"] === email)
-      const token = Math.floor(Math.random() * 100);
-      client["Token"] = token;
-      localStorage.setItem("loggedInUser", email)
-      localStorage.setItem("clientToken", JSON.stringify(clientToken));
-      window.location.href = "../client-dashboard/index.html";
+  if (role === 'Select Role') {
+    error.innerHTML = 'Please select your role';
+    return null;
+  }
+
+  if (role === 'Staff' || role === 'Admin') {
+    const staffToken = JSON.parse(localStorage.getItem('staffToken')) || [];
+    const staff = {};
+    staff.email = email;
+    staff.role = role;
+    staffToken.push(staff);
+    localStorage.setItem('staffToken', JSON.stringify(staffToken));
+    localStorage.setItem('loggedInStaff', email);
+
+    window.location.href = '../staff-dashboard/index.html';
+  }
+
+  if (role === 'Client') {
+    const clientToken = JSON.parse(localStorage.getItem('clientToken')) || [];
+    if (clientToken.some(item => item['Email'] === email)) {
+      if (clientToken.find(item => item['Password'] === password)) {
+        const client = clientToken.find(x => x['Email'] === email);
+        const token = Math.floor(Math.random() * 100);
+        client['Token'] = token;
+        localStorage.setItem('loggedInUser', email);
+        localStorage.setItem('clientToken', JSON.stringify(clientToken));
+        window.location.href = '../client-dashboard/index.html';
+      } else {
+        error.innerHTML = 'Incorrect password';
+      }
     } else {
-      error.innerHTML = "Incorrect password"
+      error.innerHTML = 'User does not exist. Do you mean to sign up?';
     }
-  } else {
-    error.innerHTML = "User does not exist. Do you mean to sign up?";
   }
 }
--- staff-dashboard/script.js
+++ staff-dashboard/script.js
@@ -1,32 +1,31 @@
 (function() {
-  const accounts = document.getElementById("accounts");
-  const staff = JSON.parse(localStorage.getItem("staffToken")) || [];
-  const admin = document.getElementById("admin");
-  const email = localStorage.getItem("loggedInStaff");
+  const accounts = document.getElementById('accounts');
+  const staff = JSON.parse(localStorage.getItem('staffToken')) || [];
+  const admin = document.getElementById('admin');
+  const email = localStorage.getItem('loggedInStaff');
 
   if (!email) {
-    window.location.href = "../staff-login/index.html";
+    window.location.href = '../staff-login/index.html';
   }
 
   const user = staff.find(x => x.email === email);
-  if (user.role === "Admin") {
-    admin.style.display = "block";
+  if (user.role === 'Admin') {
+    admin.style.display = 'block';
   }
 
-  const clients = JSON.parse(localStorage.getItem("clientToken")) || [];
+  const clients = JSON.parse(localStorage.getItem('clientToken')) || [];
   clients.map(function(item) {
-    const ul = document.createElement("ul");
-    ul.setAttribute("class", "list");
-    accounts.appendChild(ul);
-    if (item["Account Number"]) {
-      const li = document.createElement("li");
-      li.setAttribute("class", "item");
-      li.innerHTML = item["Account Number"];
+    const ul = document.createElement('ul');
+    ul.setAttribute('class', 'list');
+    if (item['Account Number'] && item['Account Number'].length > 1) {
+      accounts.appendChild(ul);
+      const li = document.createElement('li');
+      li.setAttribute('class', 'item');
+      li.innerHTML = item['Account Number'];
       ul.appendChild(li);
-
       li.onclick = function() {
-        localStorage.setItem("acc", JSON.stringify(item["Account Number"]));
-        window.location.href = "../account-record/index.html";
+        localStorage.setItem('acc', JSON.stringify(item['Account Number']));
+        window.location.href = '../account-record/index.html';
       };
     }
   });
