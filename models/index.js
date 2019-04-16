c6ad5a3ef9a5b89669dd22123f4c64d068335bf6
Chukwualuka Chiama <lukaschiama@gmail.com>
2019-04-16 09:27:55 +0100

    modify modelsfor db

--- models/index.js
+++ models/index.js
@@ -8,13 +8,13 @@ const create = async (data) => {
   return newItem;
 };
 
-const findAll = async () => {
-  const results = await pool.query('SELECT * FROM users ORDER BY id ASC');
+const findAll = async (tableName) => {
+  const results = await pool.query('SELECT * FROM $1 ORDER BY id ASC', [tableName]);
   return results;
 };
 
 const findOne = async (param) => {
-  const result = await pool.query('SELECT * FROM users WHERE id = $1', [param]);
+  const result = await pool.query('SELECT * FROM $1 WHERE id = $1', [param]);
   return result;
 };
 
